9e8c94f355251e95c025fb57a844b4b941af8163
ZeeJay321 <zain.ul.abidin@qbatch.com>
2025-10-16 12:38:27 +0500

    Stripe/ New Requirements testing

--- services/stripe/index.ts
+++ services/stripe/index.ts
@@ -57,7 +57,8 @@ export async function createCheckoutSession({
     line_items: lineItems,
     payment_intent_data: {
       receipt_email: email,
-      description: 'Order payment via E-Commerce App'
+      description: 'Order payment via E-Commerce App',
+      setup_future_usage: 'off_session'
     },
     saved_payment_method_options: {
       allow_redisplay_filters: ['always']
